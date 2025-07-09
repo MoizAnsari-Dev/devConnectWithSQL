585b3ebe4ddac4efef0b7ce57900b47007b424ba
Moiz Ansari <moizansari.india@gmail.com>
2025-07-09 20:21:50 +0530

    Test and fixing some code

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -14,7 +14,9 @@ router.post("/", async (req, res) => {
       `INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *`,
       [name, email.toLowerCase(), password]
     );
-    console.log(results.rows);
+    console.log(results.rows.length);
+    console.log("Users Register successfully");
+    
 
     res.json(results.rows[0]);
   } catch (error) {
